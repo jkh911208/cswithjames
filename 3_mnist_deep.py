932e97cdd4f178c2415fa839548351f8bde3afca
gyuhyong <jkh911208@naver.com>
2017-12-29 21:19:50 -0600

    edit

--- 3_mnist_deep.py
+++ 3_mnist_deep.py
@@ -4,49 +4,49 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
 
-batch_size = 128
+# Hyperparameters
+batch_size = 500
 num_classes = 10
 epochs = 10
 
+# Load Mnist Data
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-img_height, img_width, channel = x_train.shape[1],x_train.shape[2], 1
-#print(data_size,img_height, img_width, channel)
-
-input_shape = (img_height, img_width, channel,None)
-
+img_height, img_width = x_train.shape[1],x_train.shape[2]
 
 # convert to one hot encoing 
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
-# input shape
-#print(x_train.shape)
-x_train = x_train.reshape(x_train.shape[0], img_height*img_width*channel)
-x_test = x_test.reshape(x_test.shape[0], img_height*img_width*channel)
-#print(x_train.shape)
-#exit(0)
+# Flatten the image
+x_train = x_train.reshape(x_train.shape[0], img_height*img_width)
+x_test = x_test.reshape(x_test.shape[0], img_height*img_width)
 
+# Define the Model
 model = Sequential()
-model.add(Dense(32, activation='softmax', input_shape=(784,)))
-model.add(Dense(32, activation='softmax'))
-model.add(Dense(32, activation='softmax'))
-model.add(Dense(32, activation='softmax'))
-model.add(Dense(32, activation='softmax'))
+model.add(Dense(32, activation='sigmoid', input_shape=(784,)))
+model.add(Dense(32, activation='sigmoid'))
+model.add(Dense(32, activation='sigmoid'))
+model.add(Dense(32, activation='sigmoid'))
+model.add(Dense(32, activation='sigmoid'))
+# 3 -> 6 Layers, Deeper Network
 model.add(Dense(num_classes, activation='softmax'))
 
+# print the model summary
 model.summary()
 
+# determine Loss function and Optimizer
 model.compile(loss='categorical_crossentropy',
               optimizer=Adam(),
               metrics=['accuracy'])
 
-history = model.fit(x_train, y_train,
+# Train the Model
+model.fit(x_train, y_train,
                     batch_size=batch_size,
                     epochs=epochs,
                     verbose=1,
                     validation_data=(x_test, y_test))
 
+# Test the model
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
